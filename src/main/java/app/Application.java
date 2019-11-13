44ad94739ceec46fea860ca68e8122a6136b3c01
alena.kosova <a.kosova94@gmail.com>
2019-11-13 17:13:30 +0300

    added login form

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,7 +1,6 @@
 package app;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +9,9 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+//    }
 
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx){
